24a804ab5b7e0bde1b44c19f3891f0fe204a3b43
Ryan McGinty <rpmcginty@users.noreply.github.com>
2025-07-02 14:44:27 -0700

    hotfix test for version was not updated

--- test/aibs_informatics_core/utils/test_version.py
+++ test/aibs_informatics_core/utils/test_version.py
@@ -3,4 +3,4 @@ from aibs_informatics_core.utils.version import get_version
 
 def test__version__is_correct():
     # just checking major.minor version
-    assert get_version("aibs_informatics_core").startswith("0.0.")
+    assert get_version("aibs_informatics_core").startswith("0.1.")
